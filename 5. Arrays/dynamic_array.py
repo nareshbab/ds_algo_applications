43fcd102617839044c9e18aeb15f3a53b614f299
Nareshbab <nareshbabral@gmail.com>
2021-09-13 15:36:24 +0530

    added array insert and remove methods

--- 5. Arrays/dynamic_array.py
+++ 5. Arrays/dynamic_array.py
@@ -36,3 +36,25 @@ class DynamicArray:
 	def _make_array(self, c):
 		"""Returns a new array of length c"""
 		return (c * ctypes.py_object)()
+
+	def insert(self, k, value):
+		"""Inserts a value at specific index"""
+		if self._n == self._capacity:
+			self._resize(1 * self._capacity)
+		for i in range(self._n, k, -1):         # First make space for new element
+			self._A[i] = self._A[i-1]           # Shift all elements from k to n-1 by 1
+		self._A[k] = value
+		self._n += 1
+
+	def remove(self, value):
+		"""Removes the first occurrence of the element
+		We do not shrink the array in this method
+		"""
+		for i in range(self._n):
+			if self._A[i] == value:
+				for j in range(i, self._n - 1):
+					self._A[j] = self._A[j+1]
+				self._A[self._n - 1] = None
+				self._n -= 1
+				return
+		raise ValueError("Value not found")
