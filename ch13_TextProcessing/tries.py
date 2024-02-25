3e4977c2d8e426db36e72292b3d2d26e85cc4f54
Naresh <nareshbabral@gmail.com>
2024-02-25 20:19:43 +0530

    added longest word with prefix solution

--- ch13_TextProcessing/tries.py
+++ ch13_TextProcessing/tries.py
@@ -1,6 +1,8 @@
 
 ## ---------- TRIES --------------##
 
+# Total nodes of trie =  count of unique prefixes
+
 class TrieNode:
 
     def __init__(self) -> None:
@@ -18,6 +20,9 @@ class Trie:
 
     def getNode(self):
         return TrieNode()
+    
+    def getRoot(self):
+        return self.root
     
     def _charToIndex(self, ch):
         return ord(ch) - ord('a')
@@ -57,6 +62,73 @@ class Trie:
 
         return root.is_eow
     
+    def count_nodes(self, root) -> int:        
+        count = 0
+        for i in range(26):
+            if root.children[i]:
+                count += self.count_nodes(root.children[i])
+        return count + 1
+    
+    def word_break(self, key):
+
+        if len(key) == 0:
+            return True
+        
+        for i in range(1,len(key)+1):
+            first_part = key[0:i]
+            second_part = key[i:len(key)]
+
+            if self.search(first_part) and self.word_break(second_part):
+                return True
+        
+        return False
+    
+"""
+Substrings of a string have a unique property i.e
+all substrings = all prefixes of all suffixes
+
+for eg. lets say string = "ababa"
+Suffixes -> Prefixes
+    ababa -> a, ab, aba, abab, ababa
+        baba -> b, ba, bab, baba
+        aba -> a, ab, aba
+        ba -> b, ba
+        a -> a
+        "" -> ""
+"""
+def count_unique_substrings(key):
+    
+    suffixes = []
+
+    for i in range(len(key)):
+        suffixes.append(key[i:])
+
+    t = Trie()
+
+    for suffix in suffixes:
+        t.insert(suffix)
+
+    root = t.getRoot()    
+    return t.count_nodes(root)
+    
+
+"""
+Longest word with all prefixes:
+We know that nodes in trie represents all the prefixes
+so we want to traverse a branch which is longest and all nodes are eow
+"""
+def longest_word_prefix(root, temp):
+    global longest_word
+
+    for i in range(26):
+        if root.children[i] and root.children[i].is_eow:
+            temp += chr(i + ord('a'))
+            if len(temp) > len(longest_word):
+                longest_word =  temp
+            longest_word_prefix(root.children[i], temp)
+            temp = temp[:-1]
+
+
 
 if __name__ == '__main__':
     keys = ["the", "a", "there", "anaswe", "any", "by", "their"]
@@ -76,4 +148,27 @@ if __name__ == '__main__':
     print("{} ------- {}".format("thaw", output[t.search("thaw")]))
     print("{} ------- {}".format("an", output[t.search("an")]))
 
+    keys2 = ["i", "like", "sam", "samsung", "mobile"]
+    t2 = Trie()
+    for key in keys2:
+        t2.insert(key)
+    print(t2.word_break("ilikesamsung"))
 
+
+    ## Counting unique substrings
+    keys3 = "apple"
+
+    print(count_unique_substrings(keys3))
+
+
+    keys4 = ["a", "banana", "app", "appl", "ap", "apply", "apple"]
+
+    t3 = Trie()
+
+    for word in keys4:
+        t3.insert(word)
+
+    t3_root = t3.getRoot()
+    longest_word = ""
+    longest_word_prefix(t3_root, "")
+    print(longest_word)
